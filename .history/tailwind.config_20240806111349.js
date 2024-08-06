5cf3d11a26a90a3a350fc30f806e1e0b5a4664be
Joshua Mance <75736470+JoshMance@users.noreply.github.com>
2024-08-06 13:34:23 +0800

    Design tidy-up

---
+++ .history/tailwind.config_20240806111349.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.html', 
+            './index.html'],
+  theme: {
+    container: {
+      center: true,
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'primary': '#000319', 
+      'secondary':'#041B29',
+      'accent': '#11EF79',
+      'white': '#F1F3F6',
+      'light-grey': '#8D8D92',
+      'grey' : '#171718'
+    },
+  },
+  plugins: [],
+}
+
