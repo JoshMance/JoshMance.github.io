ebb7b6e84478ff20d94c3c16a61864308397b241
Joshua Mance <75736470+JoshMance@users.noreply.github.com>
2024-12-14 21:53:36 +0800

    Restarting the my skills section

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,54 +4,27 @@ module.exports = {
             './index.html'],
   darkMode: 'selector',
   theme: {
-    extend: {
-      animation: {
-        "text-reveal": "text-reveal 1.2s cubic-bezier(0.25, 1, 0.5, 1)",
-        "opacity-reveal": "opacity-reveal 1.2s cubic-bezier(0.25, 1, 0.5, 1)",
-      },
-      keyframes: {
-
-        "text-reveal": {
-          "0%": {
-            transform: "translate(0, 100%)",
-            opacity: 1,
-          },
-          "100%": {
-            transform: "translate(0, 0)",
-            opacity: 1,
-          },
-        },
-
-      "opacity-reveal": {
-          "0%": {
-            opacity: 0.3,
-          },
-          "100%": {
-            opacity: 1,
-          },
-        },
-      },
-    },
     container: {
       center: true,
     },        
     colors: {
       transparent: 'transparent',
       current: 'currentColor',
-      'primary': '#05306b', 
-      'secondary':'#114794',
-      'accent': '#ffe600',
+      'primary': '#152342', 
+      'secondary':'#1E3056',
+      'accent': '#ddc700',
       'accent-dull': '#ffde59',
 
-      'primary-dark': '#011931', 
+      'primary-dark': '#0e1729', 
       'secondary-dark':'#013568',
       'accent-dark': '#806c00',
 
       'primary-faint': '#7aa0bfff', 
 
-      'black':'#06183e',
-      'grey': "#ffffff75",
-      'white': '#F9F7F7',
+      'black':'#201f1f',
+      'dark-grey':'#757579',
+      'grey': "#f4f4f4",
+      'white': '#ffffff',
     },
   },
   plugins: [       
